786bef487e305495d8dc65951f16efe6e3a7d28d
er-FAN <erfanbarbari1382@outlook.com>
2026-08-26 13:17:36 +0330

    Add hasComponent arrays to track which components each entity has

--- step-0001-ecs-entity-definition/include/Storage.hpp
+++ step-0001-ecs-entity-definition/include/Storage.hpp
@@ -10,7 +10,7 @@
 
 namespace step_0001
 {
-
+    const std::uint32_t MAX_ENTITIES = 1000;
     /**
      * @brief Central data container holding component maps for all entities.
      * Uses struct for direct and zero-overhead data access by Systems.
@@ -19,33 +19,41 @@ namespace step_0001
     {
         // --- Physical Components ---
         Position positions[MAX_ENTITIES];
+        bool hasPosition[MAX_ENTITIES]{};
         Size sizes[MAX_ENTITIES];
+        bool hasSize[MAX_ENTITIES]{};
         Mass masses[MAX_ENTITIES];
+        bool hasMass[MAX_ENTITIES]{};
 
         // --- Movement Components ---
         Velocity velocities[MAX_ENTITIES];
+        bool hasVelocity[MAX_ENTITIES]{};
 
         // --- Render & Identity Components ---
         Color colors[MAX_ENTITIES];
+        bool hasColor[MAX_ENTITIES]{};
         Owner owners[MAX_ENTITIES];
+        bool hasOwner[MAX_ENTITIES]{};
 
         // --- Vehicle Components ---
         Capacity capacities[MAX_ENTITIES];
+        bool hasCapacity[MAX_ENTITIES]{};
         Specification specifications[MAX_ENTITIES];
+        bool hasSpecification[MAX_ENTITIES]{};
 
         // --- Ball Components ---
         Ball balls[MAX_ENTITIES];
+        bool hasBall[MAX_ENTITIES]{};
     };
 
-    struct EntityStorage 
+    struct EntityStorage
     {
-        Entity entities[MAX_ENTITIES];
+        std::vector<Entity> entities;
     };
 
     struct SystemStorage
     {
         std::vector<std::unique_ptr<System>> systems;
     };
-    
 
 } // namespace step_0001
